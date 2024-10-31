9a74f1ccae61fee92ff1460445bd4c722f0552e3
Shyam Goli <golishyamgolisai@gmail.com>
2024-10-31 20:10:02 +0530

    added transpose to the matrix

--- LinearRegression_test.py
+++ LinearRegression_test.py
@@ -8,7 +8,7 @@ import numpy as np
 def mse(y_pred, y):
     return np.mean((y_pred - y)**2)
 
-X, y = datasets.make_regression(n_samples=500, n_features=1, noise= 50, random_state= 31)
+X, y = datasets.make_regression(n_samples=500, n_features=2, noise= 50, random_state= 31)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 31)
 
 lr_model = LinearRegression()
@@ -17,11 +17,11 @@ y_pred = lr_model.predict(X_test)
 
 print("Accuracy = ", mse(y_pred, y_test))
 
-fig = plt.figure(figsize=(8, 6))
-plt.scatter(x = X_train[:, 0], y=y_train, color = 'green')
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_test, y_pred, color = 'red', label='prediction')
-plt.show()
+# fig = plt.figure(figsize=(8, 6))
+# plt.scatter(x = X_train[:, 0], y=y_train, color = 'green')
+# plt.scatter(X_test, y_test, color = 'red')
+# plt.plot(X_test, y_pred, color = 'red', label='prediction')
+# plt.show()
 # print(X[:, :5])
 # print(X_train[:, 0])
 # print(X)
--- my_ML_models/LinearRegression.py
+++ my_ML_models/LinearRegression.py
@@ -9,12 +9,15 @@ class LinearRegression:
 
     def fit(self, X, y, lr = 0.01):
         data_size , n_features = X.shape
+        print(X.shape)
+        print(y.shape)
         self.weights = np.zeros(n_features)
         self.bias = 0
 
         for i in range(self.n_iters):
             y_pred = np.dot(X, self.weights) + self.bias
-            dw = (1/data_size)* np.dot(X.reshape(1,-1), (y_pred - y))
+            # dw = (1/data_size)* np.dot(X.reshape(n_features,-1), (y_pred - y))
+            dw = (1/data_size)* np.dot(X.T, (y_pred - y))
             db = (1/data_size) * np.sum(y_pred - y)
 
             self.weights = self.weights - lr * dw
